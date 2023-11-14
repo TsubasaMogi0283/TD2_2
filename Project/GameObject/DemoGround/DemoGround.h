49da627417844604585e26e3de008e9675b48300
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2023-11-14 11:59:19 +0900

    DemoGroundクラス追加

--- Project/GameObject/DemoGround/DemoGround.h
+++ Project/GameObject/DemoGround/DemoGround.h
@@ -1,1 +1,19 @@
 #pragma once
+
+#include "Polygon/Model/Model.h"
+#include "WorldTransform/WorldTransform.h"
+
+
+
+
+/* DemoGroundクラス */
+class Demoground {
+
+public:
+
+private:
+
+private:
+
+
+};
