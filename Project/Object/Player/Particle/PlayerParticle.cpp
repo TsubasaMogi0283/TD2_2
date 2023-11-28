a72be93d6534a0bb2290f625f40cfb0e218e1622
k022g0114@g.neec.ac.jp <k022g0114@g.neec.ac.jp>
2023-11-28 12:16:43 +0900

    PlayerParticle追加objも

---
+++ Project/Object/Player/Particle/PlayerParticle.cpp
@@ -0,0 +1,31 @@
+#include "PlayerParticle.h"
+
+
+
+/// <summary>
+/// 初期化処理
+/// </summary>
+void PlayerPerlicle::Initialize() {
+
+
+}
+
+
+
+/// <summary>
+/// 更新処理
+/// </summary>
+void PlayerPerlicle::Update() {
+
+
+}
+
+
+
+/// <summary>
+/// 描画処理
+/// </summary>
+void PlayerPerlicle::Draw() {
+
+
+}
--- Project/Object/Player/Particle/PlayerParticle.h
+++ Project/Object/Player/Particle/PlayerParticle.h
@@ -1,1 +1,76 @@
 #pragma once
+
+#include "Input/Input.h"
+#include "Polygon/Model/Model.h"
+#include "WorldTransform/WorldTransform.h"
+
+
+
+
+// Playerの前方宣言
+class Player;
+
+
+struct PlayerParticleProperty {
+	std::unique_ptr<Model> model = nullptr;
+	Transform transform{};
+	Vector3 velocity{};
+	Vector3 size{};
+	Vector4 color{};
+};
+struct PlayerParticleEmitter {
+	Transform transform{};
+};
+
+
+
+/* PlayerParticleクラス */
+class PlayerPerlicle {
+
+public: // メンバ関数
+
+	PlayerPerlicle() {};
+	~PlayerPerlicle() {};
+
+	/// <summary>
+	/// 初期化処理
+	/// </summary>
+	void Initialize();
+
+	/// <summary>
+	/// 更新処理
+	/// </summary>
+	void Update();
+
+	/// <summary>
+	/// 描画処理
+	/// </summary>
+	void Draw();
+
+
+#pragma region Get
+
+
+
+#pragma endregion
+
+
+#pragma region Set
+
+
+
+#pragma endregion
+
+
+private: // メンバ関数
+
+
+private: // メンバ変数
+
+	// プレイヤーパーティクル
+	PlayerParticleProperty particle_;
+
+	// エミッター
+	PlayerParticleEmitter emitter_;
+
+};
